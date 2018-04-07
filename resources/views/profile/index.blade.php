39bc96dee68dc6bf0e21ca0e2ca09a5c4fc62e24
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-04-07 11:24:48 +0700

    profile ui

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -27,15 +27,14 @@
 								<span class="fas fa-lg fa-pencil-alt"></span>
 							</button>
 						</a>
+						<a href="{{ url('/box') }}">
+							<button class="btn btn-circle btn-main2-color">
+								<span class="fas fa-lg fa-box-open"></span>
+							</button>
+						</a>
 						<a href="{{ url('/compose') }}">
 							<button class="btn btn-circle btn-main2-color">
 								<span class="fas fa-lg fa-plus"></span>
-							</button>
-						</a>
-						<a href="{{ url('/box') }}">
-							<button class="btn btn-main2-color">
-								<span class="fas fa-lg fa-box-open"></span>
-								<span>{{ $p->ttl_bookmark }}</span>
 							</button>
 						</a>
 					@else
