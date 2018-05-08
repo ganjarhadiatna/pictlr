225dce8debb5e6f9f59df8ba821bed9644ad15b3
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-05-09 01:40:03 +0700

    fixed ui for multiple images

--- resources/views/story/index.blade.php
+++ resources/views/story/index.blade.php
@@ -154,171 +154,172 @@
 
 	});
 </script>
-<div class="sc-header">
-	<div class="sc-place pos-fix">
-		<div class="col-1000px">
-			<div class="sc-grid sc-grid-2x">
-				<div class="sc-col-1">
-					@if ($story->id == Auth::id())
-						<button class="btn btn-circle btn-main2-color btn-focus" onclick="opQuestionPost('{{ $story->idstory }}')">
-							<span class="far fa-lg fa-trash-alt"></span>
-						</button>
-						<a href="{{ url('/story/'.$story->idstory.'/edit/'.Auth::id().'/'.csrf_token()) }}">
-							<button class="btn btn-circle btn-main2-color btn-focus">
-								<span class="fas fa-lg fa-pencil-alt"></span>
-							</button>
-						</a>
-					@endif
-					<button class="btn btn-circle btn-main2-color btn-focus" onclick="opPostPopup('open', 'menu-popup', '{{ $story->idstory }}', '{{ $story->id }}', '{{ $title }}')">
-						<span class="fas fa-lg fa-ellipsis-h"></span>
-					</button>
-				</div>
-				<div class="sc-col-2 txt-right">
-					<button class="btn btn-main3-color btn-no-border"
-						key="{{ $story->idstory }}" 
-						onclick="addBookmark('{{ $story->idstory }}')">
-						@if (is_int($story->is_save))
-							<span class="bookmark-{{ $story->idstory }} fas fa-lg fa-bookmark" id="bookmark-{{ $story->idstory }}"></span>
-						@else
-							<span class="bookmark-{{ $story->idstory }} far fa-lg fa-bookmark" id="bookmark-{{ $story->idstory }}"></span>
-						@endif
-						<span>Save</span>
-					</button>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 <div class="place-story">
 	<div class="main">
 		<div class="place">
-			<div class="frame-story grid-2x" id="main-story">
-				<div class="grid-1">
-					<div class="pos mid">
-						<div class="pict padding-bottom-15px">
-							<img src="{{ asset('/story/covers/'.$story->cover) }}" id="place-img" alt="pict">
+			<div class="frame-story">
+				<div class="sc-header padding-bottom-5px">
+					<div class="sc-place no-background col-full">
+						<div class="sc-grid sc-grid-2x">
+							<div class="sc-col-1">
+								@if ($story->id == Auth::id())
+									<button class="btn btn-circle btn-primary-color  btn-focus" onclick="opQuestionPost('{{ $story->idstory }}')">
+										<span class="far fa-lg fa-trash-alt"></span>
+									</button>
+									<a href="{{ url('/story/'.$story->idstory.'/edit/'.Auth::id().'/'.csrf_token()) }}">
+										<button class="btn btn-circle btn-primary-color  btn-focus">
+											<span class="fas fa-lg fa-pencil-alt"></span>
+										</button>
+									</a>
+								@endif
+								<button class="btn btn-circle btn-primary-color  btn-focus" onclick="opPostPopup('open', 'menu-popup', '{{ $story->idstory }}', '{{ $story->id }}', '{{ $title }}')">
+									<span class="fas fa-lg fa-ellipsis-h"></span>
+								</button>
+							</div>
+							<div class="sc-col-2 txt-right">
+								<button class="btn btn-main3-color btn-no-border"
+									key="{{ $story->idstory }}" 
+									onclick="addBookmark('{{ $story->idstory }}')">
+									@if (is_int($story->is_save))
+										<span class="bookmark-{{ $story->idstory }} fas fa-lg fa-bookmark" id="bookmark-{{ $story->idstory }}"></span>
+									@else
+										<span class="bookmark-{{ $story->idstory }} far fa-lg fa-bookmark" id="bookmark-{{ $story->idstory }}"></span>
+									@endif
+									<span>Save</span>
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
+
 				<div class="grid-2x">
-					<div class="pos mid" key="more design">
-						<div class="ctn-main-font ctn-14px ctn-sek-color ctn-bold padding-bottom-15px">More images</div>
-						<div class="place-search-tag padding-bottom-15px">
-							<div class="st-lef">
-								<div class="btn btn-circle btn-sekunder-color btn-no-border hg-100px" onclick="toLeft()">
-									<span class="fa fa-lg fa-angle-left"></span>
-								</div>
-							</div>
-							<div class="st-mid ctn-main-font ctn-left" id="ctnTag">
-								@if (count($images) != 0)
-									@foreach ($images as $img)
-										<div 
-											class="image image-100px image-radius change-img"
-											style="background-image: url({{ asset('/story/thumbnails/'.$img->image) }})"
-											key="{{$img->image}}"
-											></div>
-									@endforeach
-								@endif
-							</div>
-							<div class="st-rig">
-								<div class="btn btn-circle btn-sekunder-color btn-no-border hg-100px" onclick="toRight()">
-									<span class="fa fa-lg fa-angle-right"></span>
-								</div>
+					<div class="grid-1">
+						<div class="mid">
+							<div class="pict padding-bottom-15px">
+								<img src="{{ asset('/story/covers/'.$story->cover) }}" id="place-img" alt="pict">
 							</div>
 						</div>
 					</div>
-					<div class="pos bot">
-						<div class="profile padding-bottom-15px">
-							<div class="foto">
-								<a href="{{ url('/user/'.$story->id) }}">
-									<div class="image image-45px image-circle" style="background-image: url({{ asset('/profile/thumbnails/'.$story->foto) }});"></div>
-								</a>
-							</div>
-							<div class="info">
-								<div class="name">
-									<div>
-										<a href="{{ url('/user/'.$story->id) }}">
-											{{ $story->username }}
-										</a>
+					<div class="grid-2">
+						<div class="pos mid" key="more design">
+							<div class="ctn-main-font ctn-14px ctn-sek-color ctn-bold padding-bottom-15px">More images</div>
+							<div class="place-search-tag padding-bottom-15px">
+								<div class="st-lef">
+									<div class="btn btn-circle btn-sekunder-color btn-no-border hg-100px" onclick="toLeft()">
+										<span class="fa fa-lg fa-angle-left"></span>
+									</div>
+								</div>
+								<div class="st-mid ctn-main-font ctn-left" id="ctnTag">
+									@if (count($images) != 0)
+										@foreach ($images as $img)
+											<div 
+												class="image image-100px image-radius change-img"
+												style="background-image: url({{ asset('/story/thumbnails/'.$img->image) }})"
+												key="{{$img->image}}"
+												></div>
+										@endforeach
+									@endif
+								</div>
+								<div class="st-rig">
+									<div class="btn btn-circle btn-sekunder-color btn-no-border hg-100px" onclick="toRight()">
+										<span class="fa fa-lg fa-angle-right"></span>
 									</div>
 								</div>
 							</div>
-							<div class="tool">
-								@if ($story->id != Auth::id())
-									@if (is_int($statusFolow))
-										<input type="button" name="follow" class="btn btn-main3-color" id="add-follow-{{ $story->id }}" value="Unfollow" onclick="opFollow('{{ $story->id }}', '{{ url("/") }}', '{{ Auth::id() }}')">
-									@else
-										<input type="button" name="follow" class="btn btn-sekunder-color" id="add-follow-{{ $story->id }}" value="Follow" onclick="opFollow('{{ $story->id }}', '{{ url("/") }}', '{{ Auth::id() }}')">
+						</div>
+						<div class="pos bot">
+							<div class="profile padding-bottom-15px">
+								<div class="foto">
+									<a href="{{ url('/user/'.$story->id) }}">
+										<div class="image image-45px image-circle" style="background-image: url({{ asset('/profile/thumbnails/'.$story->foto) }});"></div>
+									</a>
+								</div>
+								<div class="info">
+									<div class="name">
+										<div>
+											<a href="{{ url('/user/'.$story->id) }}">
+												{{ $story->username }}
+											</a>
+										</div>
+									</div>
+								</div>
+								<div class="tool">
+									@if ($story->id != Auth::id())
+										@if (is_int($statusFolow))
+											<input type="button" name="follow" class="btn btn-main3-color" id="add-follow-{{ $story->id }}" value="Unfollow" onclick="opFollow('{{ $story->id }}', '{{ url("/") }}', '{{ Auth::id() }}')">
+										@else
+											<input type="button" name="follow" class="btn btn-sekunder-color" id="add-follow-{{ $story->id }}" value="Follow" onclick="opFollow('{{ $story->id }}', '{{ url("/") }}', '{{ Auth::id() }}')">
+										@endif
 									@endif
-								@endif
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="pos mid">
-						<div>
-							@if ($story->description != "")
-								<div class="content ctn-main-font ctn-sans-serif padding-bottom-15px">
-									<?php echo $story->description; ?>
+						<div class="pos mid">
+							<div>
+								@if ($story->description != "")
+									<div class="content ctn-main-font ctn-sans-serif padding-bottom-15px">
+										<?php echo $story->description; ?>
+									</div>
+								@endif
+							</div>
+							@if (count($tags) > 0)
+								<div class="padding-bottom-15px">
+									@foreach($tags as $tag)
+									<?php 
+										$replace = array('[',']','@',',','.','#','+','-','*','<','>','-','(',')',';','&','%','$','!','`','~','=','{','}','/',':','?','"',"'",'^');
+										$title = str_replace($replace, '', $tag->tag); 
+									?>
+									<a href="{{ url('/tags/'.$title) }}" class="frame-top-tag">
+										<div>{{ $tag->tag }}</div>
+									</a>
+									@endforeach
 								</div>
 							@endif
 						</div>
-						@if (count($tags) > 0)
-							<div class="padding-bottom-15px">
-								@foreach($tags as $tag)
-								<?php 
-									$replace = array('[',']','@',',','.','#','+','-','*','<','>','-','(',')',';','&','%','$','!','`','~','=','{','}','/',':','?','"',"'",'^');
-									$title = str_replace($replace, '', $tag->tag); 
-								?>
-								<a href="{{ url('/tags/'.$title) }}" class="frame-top-tag">
-									<div>{{ $tag->tag }}</div>
-								</a>
-								@endforeach
-							</div>
-						@endif
-					</div>
-					<div class="pos mid">
-						<div class="grid grid-2x padding-bottom-20px">
-							<div class="grid-1">
-								<span class="btn btn-sekunder-color btn-no-border btn-no-pad">
-									<span id="ttl-view">{{ ($story->views + $story->ttl_comment) }} Notes</span>
-								</span>
-							</div>
-							<div class="grid-2 text-right crs-default">
-								<span class="btn btn-main4-color">
-									<span class="fas fa-lg fa-align-center"></span>
-									<span id="ttl-view">{{ $story->views }}</span>
-								</span>
-								<span class="btn btn-main4-color" onclick="toComment()">
-									<span class="far fa-lg fa-comment"></span>
-									<span class="ttl-loves">{{ $story->ttl_comment }}</span>
-								</span>
-								<span class="btn btn-main4-color">
-									<span class="far fa-lg fa-bookmark"></span>
-									<span class="ttl-loves">{{ $story->ttl_save }}</span>
-								</span>
+						<div class="pos mid">
+							<div class="grid grid-2x padding-bottom-20px">
+								<div class="grid-1">
+									<span class="btn btn-sekunder-color btn-no-border btn-no-pad">
+										<span id="ttl-view">{{ ($story->views + $story->ttl_comment) }} Notes</span>
+									</span>
+								</div>
+								<div class="grid-2 text-right crs-default">
+									<span class="btn btn-main4-color">
+										<span class="fas fa-lg fa-align-center"></span>
+										<span id="ttl-view">{{ $story->views }}</span>
+									</span>
+									<span class="btn btn-main4-color" onclick="toComment()">
+										<span class="far fa-lg fa-comment"></span>
+										<span class="ttl-loves">{{ $story->ttl_comment }}</span>
+									</span>
+									<span class="btn btn-main4-color">
+										<span class="far fa-lg fa-bookmark"></span>
+										<span class="ttl-loves">{{ $story->ttl_save }}</span>
+									</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="pos bot">
-						<div class="padding-bottom-20px">
-							<div class="top-comment" id="tr-comment">
-								@if (Auth::id())
-								<form method="post" action="javascript:void(0)" id="comment-publish">
-									<div class="comment-head bdr-bottom">
-										<div>
-											<input class="txt comment-text txt-primary-color" id="comment-description" placeholder="Type comment here.." />
+						<div class="pos bot">
+							<div class="padding-bottom-20px">
+								<div class="top-comment" id="tr-comment">
+									@if (Auth::id())
+									<form method="post" action="javascript:void(0)" id="comment-publish">
+										<div class="comment-head bdr-bottom">
+											<div>
+												<input class="txt comment-text txt-primary-color" id="comment-description" placeholder="Type comment here.." />
+											</div>
 										</div>
-									</div>
-								</form>
-								@endif
-								<div class="comment-content" id="place-comment"></div>
-							</div>
-							<div class="frame-more" id="frame-more-comment">
-								<input type="hidden" name="offset" id="offset-comment" value="0">
-								<input type="hidden" name="limit" id="limit-comment" value="0">
-								<button class="btn btn-sekunder-color btn-radius" id="load-more-comment">
-									<span class="Load More Comment">Load More</span>
-								</button>
+									</form>
+									@endif
+									<div class="comment-content" id="place-comment"></div>
+								</div>
+								<div class="frame-more" id="frame-more-comment">
+									<input type="hidden" name="offset" id="offset-comment" value="0">
+									<input type="hidden" name="limit" id="limit-comment" value="0">
+									<button class="btn btn-sekunder-color btn-radius" id="load-more-comment">
+										<span class="Load More Comment">Load More</span>
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
